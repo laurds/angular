0e4402d66f1af6cb09cbd28177e410e536884edd
Laura Rocha <laura.matos@dbserver.com.br>
2025-07-10 18:40:42 -0300

    init main

---
+++ .angular/cache/19.2.15/estudos-angular/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,17 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-VSO73HAG.js";
+import "./chunk-KTR2ND5P.js";
+import "./chunk-6SZJNWHU.js";
+import "./chunk-FFZIAYYX.js";
+import "./chunk-6Q4RANH6.js";
+import "./chunk-CXCX2JKZ.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
